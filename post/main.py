ade4d521dcef170b28e4af0952b6600a2da19745
cassiano yasumitsu <cassiano@usuarioincomum.com>
2024-02-04 00:25:09 +0900

    feat: post crud

--- post/main.py
+++ post/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
 from . import schemas, models
@@ -17,10 +17,47 @@ def get_db():
         db.close()
 
 
-@app.post("/posts")
-async def create(post: schemas.Post, db: Session = Depends(get_db)):
+@app.get("/posts", status_code=status.HTTP_200_OK)
+async def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    if not posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="No posts found")
+    return posts
+
+
+@app.get("/posts/{id}", status_code=status.HTTP_200_OK)
+async def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post id: {id}, not found")
+    return post
+
+
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
+async def create_post(post: schemas.Post, db: Session = Depends(get_db)):
     new_post = models.Post(**post.dict())
+    if not new_post:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error creating post")
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return new_post
+
+
+@app.put("/posts/{id}", status_code=status.HTTP_202_ACCEPTED)
+async def update_post(id: int, post: schemas.Post, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).update(post.dict(), synchronize_session=False)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post id: {id}, not found")
+    db.commit()
+    return {"detail": "Post updated successfully"}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id)
+    if not post.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post id: {id}, not found")
+    post.delete(synchronize_session=False)
+    db.commit()
+    return "Post deleted successfully"
